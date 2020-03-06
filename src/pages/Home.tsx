87d1cacec591f61b1f1a7b0287531248e9dda729
William Kwao <williamkwao@gmail.com>
2020-03-05 21:19:41 -0500

    add yelp api key file

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,26 +1,64 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import React from 'react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Home.css';
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonButton,
+  IonCard,
+  IonCardContent
+} from "@ionic/react";
+import React from "react";
+import ExploreContainer from "../components/ExploreContainer";
+import "./Home.css";
+import { gql } from "apollo-boost";
+import { useQuery } from "@apollo/react-hooks";
 
 const Home: React.FC = () => {
+  const { loading, error, data, refetch } = useQuery(BUSINESS_QUERY);
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTitle>Yelp</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
+        {loading && <h1>loading</h1>}
+        {data &&
+          data.search.business.map((bus: any) => {
+            return (
+              <IonCard>
+                <img src={bus.photos[0]}/> 
+                <IonCardContent>
+                  <h3>{bus.name}</h3>
+                </IonCardContent>
+              </IonCard>
+            );
+          })}
       </IonContent>
     </IonPage>
   );
 };
 
 export default Home;
+
+let BUSINESS_QUERY = gql`
+  {
+    search(term: "burrito", location: "san francisco") {
+      total
+      business {
+        name
+        rating
+        review_count
+        photos
+        location {
+          address1
+          city
+          state
+          country
+        }
+      }
+    }
+  }
+`;
--- src/services/apollo-client.ts
+++ src/services/apollo-client.ts
@@ -1,3 +1,12 @@
-import ApolloClient from "apollo-boost";
+import ApolloClient, { from } from "apollo-boost";
+import { cordovaHttpFetchImpl } from "./cordova-http-fetch";
+import { YELP_API_KEY } from "./keys";
 
-export let client = new ApolloClient({ uri: "", fetch: fetch });
+export let client = new ApolloClient({
+  uri: "https://api.yelp.com/v3/graphql",
+  fetch: cordovaHttpFetchImpl,
+  headers: {
+    Authorization: `Bearer ${YELP_API_KEY}`,
+    "Accept-Language": "en_US"
+  }
+});
