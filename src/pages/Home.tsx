cbe189c9d4f64794523c419fa0f38a46bb73c57e
William Kwao <williamkwao@gmail.com>
2020-03-06 22:39:24 -0500

    added search bar and functionality

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,22 +6,46 @@ import {
   IonToolbar,
   IonButton,
   IonCard,
-  IonCardContent
+  IonCardContent,
+  IonSearchbar
 } from "@ionic/react";
-import React from "react";
+import React, { useState } from "react";
 import ExploreContainer from "../components/ExploreContainer";
 import "./Home.css";
 import { gql } from "apollo-boost";
 import { useQuery } from "@apollo/react-hooks";
 
 const Home: React.FC = () => {
-  const { loading, error, data, refetch } = useQuery(BUSINESS_QUERY);
+  const [search, setSearch] = useState<string>("");
+  const [searchInputText, setSearchInputText] = useState<string>("");
+  const { loading, error, data, refetch } = useQuery(BUSINESS_QUERY, {
+    variables: { search }
+  });
+  if (error) {
+    console.log("err", error);
+  }
+
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
           <IonTitle>Yelp</IonTitle>
         </IonToolbar>
+        <form
+          onSubmit={e => {
+            e.preventDefault();
+            setSearch(searchInputText);
+            refetch();
+          }}
+        >
+          <IonSearchbar
+            inputMode="text"
+            value={searchInputText}
+            onIonChange={e => {
+              setSearchInputText(e.detail.value || "");
+            }}
+          ></IonSearchbar>
+        </form>
       </IonHeader>
       <IonContent>
         {loading && <h1>loading</h1>}
@@ -29,7 +53,7 @@ const Home: React.FC = () => {
           data.search.business.map((bus: any) => {
             return (
               <IonCard>
-                <img src={bus.photos[0]}/> 
+                <img src={bus.photos[0]} />
                 <IonCardContent>
                   <h3>{bus.name}</h3>
                 </IonCardContent>
@@ -44,8 +68,8 @@ const Home: React.FC = () => {
 export default Home;
 
 let BUSINESS_QUERY = gql`
-  {
-    search(term: "burrito", location: "san francisco") {
+  query search($search: String) {
+    search(term: $search, location: "san francisco") {
       total
       business {
         name
